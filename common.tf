0420000fa49877aa6e256d37a962313195681972
Rael Garcia <rael@redhat.com>
2025-05-07 12:44:26 +0200

    clean: rename link to the 3scale-sre github org
    Signed-off-by: Rael Garcia <rael@redhat.com>

--- common.tf
+++ common.tf
@@ -2,7 +2,7 @@
 # Resource Labels
 # ------------------------------------------------------------------------------
 module "labels" {
-  source      = "git@github.com:3scale-ops/tf-aws-label.git?ref=tags/0.1.2"
+  source      = "git@github.com:3scale-sre/tf-aws-label.git?ref=tags/0.1.2"
   project     = var.project
   environment = var.environment
   workload    = var.workload
